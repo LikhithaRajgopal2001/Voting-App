5440ff7ab42e3cf8484f998b3f0eca08f1c5386f
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-04-20 09:49:17 +0530

    Add variables for EC2 instance configuration
    Define variables for AWS EC2 deployment including region, instance type, AMI ID, and key name.

---
+++ terraform/varialbes.tf
@@ -0,0 +1,20 @@
+# variables.tf
+variable "region" {
+  description = "AWS region to deploy the EC2 instance"
+  type        = string
+  default     = "us-east-1"
+}
+variable "instance_type" {
+  description = "EC2 instance type (free tier)"
+  type        = string
+  default     = "t2.micro"
+}
+variable "ami_id" {
+  description = "Ubuntu 22.04 AMI ID for us-east-1"
+  type        = string
+  default     = "ami-0c7217cdde317cfec"
+}
+variable "key_name" {
+  description = "Your EC2 key pair name to SSH into the instance"
+  type        = string
+}
