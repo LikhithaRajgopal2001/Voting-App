453645735a498d605d75ba2024a86ed07205eb57
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-04-20 09:25:12 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,19 @@
 output "instance_public_ip" {
-  value = aws_instance.voting_server.public_ip
+  value       = aws_instance.voting_server.public_ip
+  description = "The public IP address of the EC2 instance"
 }
 
 output "instance_id" {
-  value = aws_instance.voting_server.id
+  value       = aws_instance.voting_server.id
+  description = "The ID of the EC2 instance"
+}
+
+output "vpc_id" {
+  value       = aws_vpc.voting_vpc.id
+  description = "The VPC ID for the voting application"
+}
+
+output "security_group_id" {
+  value       = aws_security_group.voting_sg.id
+  description = "The security group ID for the voting application"
 }
