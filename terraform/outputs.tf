eedbe0a6e1dc47f100d824076c661799b97bd68e
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-04-22 07:42:34 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,36 @@
-output "instance_public_ip" {
+# EC2 Public IP Output (fixed name to match deploy.yml)
+output "ec2_public_ip" {
   value       = aws_instance.voting_server.public_ip
   description = "The public IP address of the EC2 instance"
 }
 
+# EC2 Instance ID
 output "instance_id" {
   value       = aws_instance.voting_server.id
   description = "The ID of the EC2 instance"
 }
 
+# VPC ID
 output "vpc_id" {
   value       = aws_vpc.voting_vpc.id
   description = "The VPC ID for the voting application"
 }
 
+# Security Group ID
 output "security_group_id" {
   value       = aws_security_group.voting_sg.id
   description = "The security group ID for the voting application"
+}
+
+# SSH Private Key (for deploy.yml to retrieve)
+output "private_key_pem" {
+  value       = tls_private_key.voting_key.private_key_pem
+  description = "The private key for SSH access to the EC2 instance"
+  sensitive   = true
+}
+
+# SSH Key Pair Name
+output "key_pair_name" {
+  value       = aws_key_pair.voting_keypair.key_name
+  description = "The name of the SSH key pair"
 }
