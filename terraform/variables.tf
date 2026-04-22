e38b10df4aee726e66b912b190c4eb6a0c667a2a
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-04-22 07:42:04 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,27 @@
-# variables.tf
+# AWS Region
 variable "region" {
   description = "AWS region to deploy the EC2 instance"
   type        = string
   default     = "ap-south-1"
 }
+
+# EC2 Instance Type
 variable "instance_type" {
   description = "EC2 instance type (free tier)"
   type        = string
   default     = "t2.micro"
 }
+
+# AMI ID (Ubuntu 22.04 for ap-south-1)
 variable "ami_id" {
-  description = "Ubuntu 22.04 AMI ID for ap-south-1"
+  description = "Ubuntu 22.04 AMI ID for the specified region"
   type        = string
   default     = "ami-0f5ee92e2d63afc18"
 }
-#variable "key_name" {
-#  description = "Your EC2 key pair name to SSH into the instance"
- # type        = string
-#}
+
+# SSH Key Pair Name
+variable "key_name" {
+  description = "Name of the EC2 key pair for SSH access"
+  type        = string
+  default     = "voting-app-key"
+}
